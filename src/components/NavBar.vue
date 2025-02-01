fb40980a2aeb5df892d3e2ee76a03716a63b59a1
TheinZawMyo <theinzawmyo40@gmail.com>
2025-02-01 22:58:13 +0630

    fix nav bar design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,26 @@
 <template>
-    <div class="container mx-auto px-[30px] py-5 flex justify-between items-center">
-        <RouterLink to="/" class="text-2xl font-bold text-green-700">Real Time Messaging App</RouterLink>
-        <div class="flex space-x-4">
+    <div class="container mx-auto px-[30px] py-5 flex flex-col md:flex-row justify-between items-center">
+        <!-- Logo -->
+        <RouterLink to="/" class="text-2xl font-bold text-green-700 mb-3 md:mb-0">
+            Real Time Messaging App
+        </RouterLink>
+
+        <!-- Navigation Links -->
+        <div class="flex flex-row md:space-y-0 space-x-4 text-center">
             <RouterLink to="/login"
-                class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 ">
+                class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 ">
                 <span
-                    class="relative px-5 py-2.5 transition-all ease-in duration-75 text-green-500 bg-white rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                    class="relative px-5 py-2.5 transition-all ease-in duration-75 text-green-500 bg-white rounded-md group-hover:bg-transparent">
                     Login
                 </span>
             </RouterLink>
             <RouterLink to="/register"
-                class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900">
+                class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 ">
                 <span
-                    class="relative px-5 py-2.5 transition-all ease-in text-green-500 duration-75 bg-white  rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                    class="relative px-5 py-2.5 transition-all ease-in duration-75 text-green-500 bg-white rounded-md group-hover:bg-transparent">
                     Register
                 </span>
             </RouterLink>
         </div>
     </div>
-
 </template>
